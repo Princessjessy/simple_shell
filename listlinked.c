bcd466631605cafe0c098a6a42ecbbdc2c6e0711
Abdulwahab Abdulaziz <121132708+binabdool@users.noreply.github.com>
2023-05-25 22:10:39 +0100

    Simple Shell TEAM PROJECT

--- listlinked.c
+++ listlinked.c
@@ -1,116 +1,112 @@
 #include "shell.h"
-
-alias_t *add_alias_end(alias_t **head, char *name, char *value);
-void free_alias_list(alias_t *head);
-list_t *add_node_end(list_t **head, char *dir);
-void free_list(list_t *head);
-
 /**
- * add_alias_end - Adds a node to the end of a alias_t linked list.
- * @head: A pointer to the head of the list_t list.
- * @name: The name of the new alias to be added.
- * @value: The value of the new alias to be added.
+ * _strdup - function that returns a pointer to a newly allocated space
+ * @str: string given as a parameter.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ * Return: NULL if str = NULL and On success returns a pointer to
  */
-
-alias_t *add_alias_end(alias_t **head, char *name, char *value)
+char *_strdup(char *str)
 {
-	alias_t *new_node = malloc(sizeof(alias_t));
-	alias_t *last;
+	char *newstr;
+	int len;
+	int i;
 
-	if (!new_node)
-		return (NULL);
 
-	new_node->next = NULL;
-	new_node->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (!new_node->name)
+	if (str == NULL)
 	{
-		free(new_node);
 		return (NULL);
-	}
-	new_node->value = value;
-	_strcpy(new_node->name, name);
-
-	if (*head)
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new_node;
 	}
 	else
-		*head = new_node;
-
-	return (new_node);
+	{
+		len = strlen(str);
+		newstr = malloc((sizeof(char) * len) + 1);
+		if (newstr == NULL)
+		{
+			free(newstr);
+			return (NULL);
+		}
+		for (i = 0; i < len; i++)
+			newstr[i] = str[i];
+		newstr[len] = '\0';
+	}
+	return  (newstr);
 }
 
 /**
- * add_node_end - Adds a node to the end of a list_t linked list.
- * @head: A pointer to the head of the list_t list.
- * @dir: The directory path for the new node to contain.
+ * _strlen - function that returns the length of a string.
+ * @s: string.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ * Return: length of a string
  */
-
-list_t *add_node_end(list_t **head, char *dir)
+int _strlen(const char *s)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *last;
+	int len = 0;
 
-	if (!new_node)
-		return (NULL);
-
-	new_node->dir = dir;
-	new_node->next = NULL;
-
-	if (*head)
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new_node;
-	}
-	else
-		*head = new_node;
-
-	return (new_node);
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
 
 /**
- * free_alias_list - Frees a alias_t linked list.
- * @head: THe head of the alias_t list.
+ * _strncmp - function that compares two strings tell a specific index.
+ * @str1: first string
+ * @str2: second string
+ * @n: index
+ *
+ * Return: return 0 if strings are equal, and -1 if its not.
  */
-void free_alias_list(alias_t *head)
+int _strncmp(const char *str1, const char *str2, int n)
 {
-	alias_t *next;
+	int i;
 
-	while (head)
+	for (i = 0; i < n; i++)
 	{
-		next = head->next;
-		free(head->name);
-		free(head->value);
-		free(head);
-		head = next;
+		if (str1[i] == str2[i])
+		{
+			continue;
+		}
+		else
+		{
+			return (-1);
+		}
 	}
+	return (0);
 }
-
 /**
- * free_list - Frees a list_t linked list.
- * @head: The head of the list_t list.
+ * _strcat - function appends the src string to the dest string
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: a pointer to the resulting string dest
  */
-
-void free_list(list_t *head)
+char *_strcat(char *dest, char *src)
 {
-	list_t *next;
+	int i = 0;
+	int r = _strlen(dest);
 
-	while (head)
+	while (*(src + i) != '\0')
 	{
-		next = head->next;
-		free(head->dir);
-		free(head);
-		head = next;
+		*(dest + (r + i)) = *(src + i);
+		i++;
 	}
+	*(dest + (r + i)) = '\0';
+	return (dest);
+}
+/**
+ * *_strcpy -  copies the string pointed to by src including the
+ * terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		*(dest + i) = *(src + i);
+	} while (*(src + i) != '\0');
+	return (dest);
 }
