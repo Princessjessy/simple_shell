58935ad35236357d5e9374e6406c38f2d4acca7e
Abdulwahab Abdulaziz <121132708+binabdool@users.noreply.github.com>
2023-05-26 03:14:47 +0100

    Simple Shell TEAM PROJECT

--- errors_msge2.c
+++ errors_msge2.c
@@ -1,32 +1,89 @@
 #include "shell.h"
 
 /**
- *run_cmd - Funtion to parse stdin command
- *@line: string parse to command
- *Return: command
+ * file_reader - Function to read command from file
+ * @f: File
+ * @argv: Arguments
  */
-char **run_cmd(char *line)
+void file_reader(char *f, char **argv)
 {
-	char **imputs, *imput;
-	int b, buf = BUF_SIZ;
+	FILE *rd;
+	char *run = NULL;
+	size_t len = 0;
+	int c = 0;
 
-	if (line[0] == ' ' && line[my_strlen(line)] == ' ')
-		exit(0);
-	if (!line)
-		return (NULL);
-	imputs = malloc(sizeof(char *) * buf);
-	if (imputs == NULL)
+	rd = fopen(f, "r");
+	if (!rd)
 	{
-		free(imputs);
-		perror("hsh");
-		return (NULL);
+		my_error(argv, c);
+		exit(127);
 	}
-	imput = my_strtok(line, "\n\t\r\a ");
-	for (b = 0; imput; b++)
+	while ((getline(&run, &len, rd)) != -1)
 	{
-		imputs[b] = imput;
-		imput = my_strtok(NULL, "\n\t\r\a ");
+		c++;
+		run_file(run, c, rd, argv);
 	}
-	imputs[b] = NULL;
-	return (imputs);
+	if (run != NULL)
+		free(run);
+	fclose(rd);
+	exit(0);
+}
+
+/**
+ * run_file - Function to handle cmd and exec
+ * @cmd: pointer to command
+ * @c: Error
+ * @fl: File descriptor
+ * @argv: Command line arguments
+ */
+
+void run_file(char *cmd, int c, FILE *fl, char **argv)
+{
+	char **run;
+	int check = 0;
+
+	run = run_cmd(cmd);
+	if (my_strncmp(run[0], "exit", 4) == 0)
+		exit_file(run, cmd, fl);
+	else if (builtin_val(run) == 0)
+	{
+		check = process_builtin(run, check);
+		free(run);
+	}
+	else
+	{
+		check = exec_cmd(run, cmd, c, argv);
+		free(run);
+	}
+}
+
+/**
+ * exit_file - Exit for file input
+ * @input: input from a file
+ * @cmd: Parsed command
+ * @fl: File Descriptor
+ */
+
+void exit_file(char **cmd, char *input, FILE *fl)
+{
+	int check;
+	int a = 0;
+
+	if (cmd[a] == NULL)
+	{
+		free(input);
+		free(cmd);
+		fclose(fl);
+		exit(errno);
+	}
+	while (cmd[1][a])
+	{
+		if (my_isalpha(cmd[1][a++]) < 0)
+			perror("Illegal number");
+	}
+	check = my_atoi(cmd[1]);
+	free(input);
+	free(cmd);
+	fclose(fl);
+	exit(check);
 }
