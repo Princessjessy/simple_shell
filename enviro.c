49db1c2a6a60ee45ab6df34327f6a69915946fed
Abdulwahab Abdulaziz <121132708+binabdool@users.noreply.github.com>
2023-05-26 03:13:24 +0100

    Simple Shell TEAM PROJECT

--- enviro.c
+++ enviro.c
@@ -1,47 +1,32 @@
 #include "shell.h"
+
 /**
- * my_strtok - function that extracts tokens from a string
- * @st: pointer to the string
- * @tk: pointer to the delimiter
- * Return: pointer to the next token or NULL if none
+ *run_cmd - Funtion to parse stdin command
+ *@line: string parse to command
+ *Return: command
  */
-
-char *my_strtok(char *st, const char *tk)
+char **run_cmd(char *line)
 {
-	static char *t, *c;
-	unsigned int b;
+	char **inputs, *input;
+	int a, buf = BUF_SIZ;
 
-	if (st != NULL)
-		c = st;
-	t = c;
-	if (t == NULL)
+	if (line[0] == ' ' && line[my_strlen(line)] == ' ')
+		exit(0);
+	if (!line)
 		return (NULL);
-	for (b = 0; t[b] != '\0'; b++)
+	inputs = malloc(sizeof(char *) * buf);
+	if (inputs == NULL)
 	{
-		if (check_my_delime(t[b], tk) == 0)
-			break;
-	}
-	if (c[b] == '\0' || c[b] == '#')
-	{
-		c = NULL;
+		free(inputs);
+		perror("hsh");
 		return (NULL);
 	}
-	t = c + b;
-	c = t;
-	for (b = 0; c[b] != '\0'; b++)
+	input = my_strtok(line, "\n\t\r\a ");
+	for (a = 0; input; a++)
 	{
-		if (check_my_delime(t[b], tk) == 1)
-			break;
+		inputs[a] = input;
+		input = my_strtok(NULL, "\n\t\r\a ");
 	}
-	if (c[b] == '\0')
-		c = NULL;
-	else
-	{
-		c[b] = '\0';
-		c = c + b + 1;
-
-		if (*c == '\0')
-			c = NULL;
-	}
-	return (t);
+	inputs[a] = NULL;
+	return (inputs);
 }
