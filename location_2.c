cb322bfce34dea2f3fab0bd936dab03fc4b2a961
Abdulwahab Abdulaziz <121132708+binabdool@users.noreply.github.com>
2023-05-26 17:42:06 +0100

    Simple Shell TEAM PROJECT

--- location_2.c
+++ location_2.c
@@ -3,6 +3,7 @@
 /**
  *my_intlen - function to get the lenght of int
  *@n: value of the lenght
+ *
  *Return: lenght
  */
 int my_intlen(int n)
@@ -17,31 +18,32 @@ int my_intlen(int n)
 	return (count);
 }
 /**
- *my_arr_rev - function to reverse array
+ *my_arr_rev -  reverse array
  *@ar: pointer to the array
  *@m: lenght of the array
  */
 void my_arr_rev(char *ar, int m)
 {
 	char buf;
-	int a;
+	int b;
 
-	for (a = 0; a < (m / 2); a++)
+	for (b = 0; a < (m / 2); b++)
 	{
-		buf = ar[a];
-		ar[a] = ar[(m - 1) - a];
-		ar[(m - 1) - a] = buf;
+		buf = ar[b];
+		ar[b] = ar[(m - 1) - b];
+		ar[(m - 1) - b] = buf;
 	}
 }
 /**
- *my_itoa - a function that convert int to char
+ *my_itoa - a convert int to char
  *@num: the int to convert
+ *
  *Return: pointer to char
  */
 char *my_itoa(unsigned int num)
 {
 	int count = 0;
-	int a = 0;
+	int b = 0;
 	char *str;
 
 	count = my_intlen(num);
@@ -51,19 +53,20 @@ char *my_itoa(unsigned int num)
 	*str = '\0';
 	while (num / 10)
 	{
-		str[a] = (num % 10) + '0';
+		str[b] = (num % 10) + '0';
 		num /= 10;
-		a++;
+		b++;
 	}
-	str[a] = (num % 10) + '0';
+	str[b] = (num % 10) + '0';
 	my_arr_rev(str, count);
-	str[a + 1] = '\0';
+	str[b + 1] = '\0';
 	return (str);
 }
 /**
- *my_isalpha - function that check for alphabet
+ *my_isalpha -  check for alphabet
  *@s: string to be checked
- *Return: 1 or 0
+ *
+ *Return: 1 0
  */
 int my_isalpha(int s)
 {
@@ -77,14 +80,15 @@ int my_isalpha(int s)
 	}
 }
 /**
- *my_strcmp - function that compare string
+ *my_strcmp - compare string
  *@str1: first string
  *@str2: second string
+ *
  *Return: always 0
  */
 int my_strcmp(char *str1, char *str2)
 {
-	int a, count1, count2, tmp = 0;
+	int b, count1, count2, tmp = 0;
 
 	count1 = my_strlen(str1);
 	count2 = my_strlen(str2);
@@ -93,11 +97,11 @@ int my_strcmp(char *str1, char *str2)
 		return (1);
 	if (count1 != count2)
 		return (1);
-	for (a = 0; str1[a]; a++)
+	for (b = 0; str1[b]; b++)
 	{
-		if (str1[a] != str2[a])
+		if (str1[b] != str2[b])
 		{
-			tmp = str1[a] - str2[a];
+			tmp = str1[b] - str2[b];
 			break;
 		}
 		else
