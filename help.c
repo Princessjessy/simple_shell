f0eda761f53e3dffe11c8428861692abe08a83e8
Abdulwahab Abdulaziz <121132708+binabdool@users.noreply.github.com>
2023-05-26 03:26:09 +0100

    Simple Shell TEAM PROJECT

--- help.c
+++ help.c
@@ -1,110 +1,94 @@
 #include "shell.h"
 
 /**
- *my_intlen - function to get the lenght of int
- *@m: value of the lenght
- *
- *Return: lenght
+ *my_strdup - function that double a string
+ *@st: pointer to the string
+ *Return: duplicate
  */
-int my_intlen(int m)
+char *my_strdup(char *st)
 {
-	int change = 0;
+	size_t count;
+	size_t a;
+	char *st2;
 
-	while (m != 0)
-	{
-		change++;
-		m /= 10;
-	}
-	return (change);
-}
-/**
- *my_arr_rev - function to reverse array
- *@ar: pointer to the array
- *@m: lenght of the array
- */
-void my_arr_rev(char *ar, int m)
-{
-	char buf;
-	int b;
+	count = my_strlen(st);
+	st2 = malloc(sizeof(char) * (count + 1));
 
-	for (b = 0; b < (m / 2); b++)
-	{
-		buf = ar[b];
-		ar[b] = ar[(m - 1) - b];
-		ar[(m - 1) - b] = buf;
-	}
-}
-/**
- *my_itoa - a function that convert int to char
- *@num: the int to convert
- *
- *Return: pointer to char
- */
-char *my_itoa(unsigned int num)
-{
-	int change = 0;
-	int b = 0;
-	char *str;
-
-	change = my_intlen(num);
-	str = malloc(change + 2);
-	if (str == NULL)
+	if (st2 == NULL)
 		return (NULL);
-	*str = '\0';
-	while (num / 10)
+	for (a = 0; a <= count; a++)
 	{
-		str[b] = (num % 10) + '0';
-		num /= 10;
-		b++;
+		st2[a] = st[a];
 	}
-	str[b] = (num % 10) + '0';
-	my_arr_rev(str, change);
-	str[b + 1] = '\0';
-	return (str);
+	return (st2);
 }
 /**
- *my_isalpha - function that check for alphabet
- *@c: string to be checked
- *Return: 1 or 0
+ *my_strncmp - function that compare two strings
+ *@str1: pointer to the first string
+ *@str2: pointer to the second string
+ *@num: value of char compared
+ *Return: 0 on success
  */
-int my_isalpha(int c)
+int my_strncmp(const char *str1, const char *str2, size_t num)
 {
-	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	size_t a;
+
+	if (!str1)
+		return (-1);
+	for (a = 0; a < num && str2[a]; a++)
 	{
-		return (1);
+		if (str1[a] != str2[a])
+			return (1);
+	}
+	return (0);
+}
+/**
+ *my_strchr - function that check char in a string
+ *@str: pointer to the string
+ *@s: char in the string
+ *Return: pointer to the string
+ */
+char *my_strchr(char *str, char s)
+{
+
+	for (; *str != '\0'; str++)
+	{
+		if (*str == s)
+		{
+			return (str);
+		}
+	}
+	return (NULL);
+}
+/**
+ * dis_echo - Function for built-in echo function
+ * @line: Pointer to command
+ * Return: 0 Upon Success -1 Upon Failure
+ */
+
+int dis_echo(char **line)
+{
+	pid_t pid;
+	int check;
+
+	pid = fork();
+	if (pid == 0)
+	{
+	if (execve("/bin/echo", line, environ) == -1)
+	{
+		return (-1);
+	}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		return (-1);
 	}
 	else
 	{
-		return (0);
+		do {
+			waitpid(pid, &check, WUNTRACED);
+		} while (!WIFEXITED(check) && !WIFSIGNALED(check));
 	}
-}
-/**
- *my_strcmp - function that compare string
- *@str1: first string
- *@str2: second string
- *
- *Return: always 0
- */
-int my_strcmp(char *str1, char *str2)
-{
-	int b, change1, change2, tmp = 0;
-
-	change1 = my_strlen(str1);
-	change2 = my_strlen(str2);
-
-	if (str1 == NULL || str2 == NULL)
-		return (1);
-	if (change1 != change2)
-		return (1);
-	for (b = 0; str1[b]; b++)
-	{
-		if (str1[b] != str2[b])
-		{
-			tmp = str1[b] - str2[b];
-			break;
-		}
-		else
-			continue;
-	}
-	return (tmp);
+	return (1);
 }
