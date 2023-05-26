2166ff2d2350534cab3fc3b8a018e3d034342675
Abdulwahab Abdulaziz <121132708+binabdool@users.noreply.github.com>
2023-05-26 17:23:51 +0100

    Simple Shell TEAM PROJECT

--- help_2.c
+++ help_2.c
@@ -1,26 +1,29 @@
 #include "shell.h"
+
 /**
- *my_strcpy - function that copy source
+ *my_strcpy - copy source
  *@buf: pointer to the destination
  *@sc: pointer to the source
+ *
  *Return: char copied
  */
 char *my_strcpy(char *buf, char *sc)
 {
-	int a = 0;
+	int b = 0;
 
-	while (sc[a])
+	while (sc[b])
 	{
-		buf[a] = sc[a];
-		a++;
+		buf[b] = sc[b];
+		b++;
 	}
-	buf[a] = '\0';
+	buf[b] = '\0';
 	return (buf);
 }
 /**
- *my_strcat - function that concat two string
+ *my_strcat -  concat two string
  *@dest: pointer to the first string
  *@sr: pointer to the seecond string
+ *
  *Return: pointer to string
  */
 char *my_strcat(char *dest, char *sr)
@@ -41,35 +44,39 @@ char *my_strcat(char *dest, char *sr)
 	return (ct);
 }
 /**
- * dis_num - display integers using _putchar function
+ * dis_num - integers using _putchar function
  * @num: Unsigned integer
+ *
+ * Return: nothing
  */
 void dis_num(unsigned int num)
 {
-	unsigned int a = num;
+	unsigned int b = num;
 
-	if ((a / 10) > 0)
-		dis_num(a / 10);
+	if ((b / 10) > 0)
+		dis_num(b / 10);
 
-	_putchar(a % 10 + '0');
+	_putchar(b % 10 + '0');
 }
 
 /**
- * dis_int - display integers using _putchar function
+ * dis_int -  integers using _putchar function
  * @num: Integer
+ *
+ * Return: nothing 
  */
 
 void dis_int(int num)
 {
-	unsigned int a = num;
+	unsigned int b = num;
 
 	if (num < 0)
 	{
 		_putchar('-');
-		a = -a;
+		b = -b;
 	}
-	if ((a / 10) > 0)
-		dis_num(a / 10);
+	if ((b / 10) > 0)
+		dis_num(b / 10);
 
-	_putchar(a % 10 + '0');
+	_putchar(b % 10 + '0');
 }
